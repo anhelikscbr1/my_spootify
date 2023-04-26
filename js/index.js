efeee117bfd19e6c0d6dfcf979c34108f83ac19d
anhelikscbr1 <123397822+anhelikscbr1@users.noreply.github.com>
2023-04-25 21:08:11 -0600

    se trabaja en funciones

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const cardTop = document.querySelector('#cardTop').content
 const contenido = document.querySelector('#contenido')
 const fragment = document.createDocumentFragment()
+
+const btnBuscar = document.getElementById('buscador')
+
 let topTwoHundred = []
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -33,7 +36,18 @@ const creaCards = () => {
     topTwoHundred.forEach((song) =>{
         cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
-        
+        let artists = ''
+        let size = song.trackMetadata.artists.lenght
+        song.song.trackMetadata.artists.forEach((item, size) => {
+            if(index === size-1){
+                artists += item.name
+            }
+            else{
+                artists += item.name + '/'
+            }
+            
+        })
+        cardTop.querySelector('artistname').textContent = artists
 
         const clone = cardTop.cloneNode(true)
         fragment.appendChild(clone)
@@ -41,3 +55,7 @@ const creaCards = () => {
     })
     contenido.appendChild(fragment)
 }
+
+btnBuscar.addEventListener('keypress', () =>{
+    console.log('tecla', btnBuscar.value)
+})
